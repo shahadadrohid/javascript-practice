b517d7d5abfb0145f0cb47c31b0d29abf762897c
Mohammad Shahadad Hossain <shahadadrohid@gmail.com>
2022-05-30 15:50:38 +0600

    addition commit

--- module-17-practice.js
+++ module-17-practice.js
@@ -37,3 +37,52 @@ var preparingForAdmission = true;
 var haveLinkedIn = false;
 var completeWebCourse = false;
 var is_Your_Age21 = true;
+
+// 5 Addition math
+
+var milkPrice = 65;
+milkPrice = milkPrice - 10;
+milkPrice += 20;
+milkPrice--;
+console.log(milkPrice)
+
+var iceCreamPrice = 140;
+var soapPrice = 39;
+var biryaniMasallaPrice = 50;
+var honeyPrice = 250;
+
+// Num 1 way addition
+var sum = iceCreamPrice + soapPrice + biryaniMasallaPrice + honeyPrice
+console.log(sum)
+
+// Num 2 way addition 
+console.log(iceCreamPrice + soapPrice + biryaniMasallaPrice + honeyPrice)
+
+// Subtraction 
+var myMoney = 500;
+var subtraction = myMoney - sum;
+console.log(subtraction)
+
+
+var myBudget = 500;
+var milkPrice = 74;
+var milkQuantity = 4;
+
+// Multiplication 
+var milkPriceMultiplication = milkPrice * milkQuantity;
+var priceInFloat = parseFloat(milkPriceMultiplication)
+console.log(priceInFloat)
+
+// Dividing
+var totalMilkInLitter = myBudget / milkPrice;
+
+// Integer 
+// var priceInt = parseInt(totalMilkInLitter)
+var priceInt = parseInt(totalMilkInLitter)
+console.log(priceInt)
+
+
+var firstName = 'Mohammad'
+var secondName = 'Shahadad'
+var fullName = firstName + ' ' + secondName;
+console.log(fullName)
